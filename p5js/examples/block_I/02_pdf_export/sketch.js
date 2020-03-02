01cf2247f8f4783e153a3c6f97b7f8b93a2b1b12
Andreas Muxel <login@andreasmuxel.com>
2020-03-02 22:11:45 +0100

    example PDF export added

---
+++ p5js/examples/block_I/02_pdf_export/sketch.js
@@ -0,0 +1,35 @@
+// PDF exporter, see also https://github.com/zenozeng/p5.js-pdf
+// include "p5.pdf.js" and "p5.svg.js" in index.html
+let pdf;
+let exportPDF = false;
+
+// setup
+function setup() {
+
+  // canvas
+  createCanvas(1024, 768, SVG);
+  noLoop();
+
+  // create and start writing pdf
+  pdf = createPDF();
+  pdf.beginRecord();
+
+}
+
+// draw
+function draw() {
+
+  // clear background
+  background(0);
+
+  // draw whatever you like
+  for (let i = 0; i < 100; i++) {
+    noStroke();
+    fill(255);
+    rect(random(width), random(height), 10, 10);
+  }
+
+  // save PDF
+  pdf.save();
+
+}
