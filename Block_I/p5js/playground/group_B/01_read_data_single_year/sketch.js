f3e71543e8adf7bc133ed749f787e5d7080edd80
Andreas Muxel <login@andreasmuxel.com>
2020-03-27 21:05:18 +0100

    -  first example in 'playground' added

---
+++ Block_I/p5js/playground/group_B/01_read_data_single_year/sketch.js
@@ -0,0 +1,83 @@
+// data
+let file;
+let year = 2018; // change here
+let data;
+let dataYear = [];
+
+// visualization
+let counterX = 0;
+let zoom = 5.0;
+
+// preload
+function preload() {
+
+  // load json file with weather data
+
+  //file = loadJSON("data/DWD-Augsburg-2009-2019.json"); // >> run on (local) webserver
+  file = loadJSON("https://raw.githubusercontent.com/HybridThingsLab/course-interaction-design/master/Block_I/data/DWD-Augsburg-2009-2019.json");
+
+}
+
+// setup
+function setup() {
+
+  // canvas
+  createCanvas(1024, 768);
+  noLoop(); // draw loop is just executed once
+
+  // get all weather data
+  data = file.data;
+
+  for (let i = 0; i < data.length; i++) {
+
+    // get current year
+    let currentYear = data[i].MESS_DATUM.substr(0, 4);
+
+    // check if specific year
+    if (currentYear == year) {
+      // push data into array
+      dataYear.push(data[i]);
+    }
+
+  }
+
+}
+
+// draw
+function draw() {
+
+  // clear background
+  background(0);
+
+  // draw line
+  stroke(255);
+  strokeWeight(1);
+  noFill();
+  line(0, height / 2, width, height / 2);
+
+  // draw year
+  fill(255);
+  noStroke();
+  text(year, 16, 16);
+
+
+  // loop through weather data
+  for (let i = 0; i < dataYear.length; i++) {
+
+    // GET VALUE(S) OF WEATHER DATA HERE!
+    // for example get average temperature of each day (see JSON file for further data)
+    let temperature = dataYear[i].TMK;
+
+    // draw here (WHERE THE MAGIC HAPPENS!)
+    stroke(255);
+    strokeWeight(width / dataYear.length);
+    noFill();
+    line(counterX, height / 2, counterX, height / 2 - temperature * zoom);
+
+    // counter x position
+    counterX += width / dataYear.length;
+
+  }
+
+
+}
