4754a0a19ae93629674d78ffd1e05c2ad006319c
Andreas Muxel <login@andreasmuxel.com>
2020-04-27 01:12:37 +0200

    example audivisual data playground group B added

---
+++ Block_II/p5js/playground/group_B/06_dynamic_data_audiovisual/sketch.js
@@ -0,0 +1,75 @@
+// p5.sound library used > included in index.html
+// see also https://p5js.org/reference/#/libraries/p5.sound
+
+// data
+let file;
+let data;
+
+// visualization
+let counterData = 0;
+let zoom = 10.0;
+
+// audio
+let osc;
+
+// preload
+function preload() {
+
+  // load json file with weather data
+
+  //file = loadJSON("data/DWD-Augsburg-2009-2019.json"); // >> run on (local) webserver
+  file = loadJSON("https://raw.githubusercontent.com/HybridThingsLab/course-interaction-design/master/Block_I/data/DWD-Augsburg-2009-2019.json");
+
+}
+
+// setup
+function setup() {
+
+  // canvas
+  createCanvas(600, 600);
+
+  // oscillator
+  osc = new p5.TriOsc(); // set frequency and type
+  osc.amp(0.5);
+  osc.start();
+
+  // get all weather data
+  data = file.data;
+
+}
+
+// draw
+function draw() {
+
+  // clear background
+  background(0);
+
+  // show current date
+  fill(255);
+  noStroke();
+  textAlign(LEFT);
+  text(data[counterData].MESS_DATUM, 8, 16);
+
+  // show wind speed
+  let wind = data[counterData].FM;
+  fill(255);
+  noStroke();
+  rect(width / 2 - 10, height / 2, 20, -wind * zoom);
+  textAlign(RIGHT);
+  text(wind + " m/s", width / 2 + 28, height / 2 + 24);
+
+  // sonify wind speed 
+  let freq = map(wind, 0, 20, 40, 880);
+  osc.freq(freq);
+
+
+  // counter data
+  // just every x frames
+  if ((frameCount % 4) == 0) {
+    counterData++;
+  }
+  if (counterData >= data.length) {
+    counterData = 0;
+  }
+
+}
