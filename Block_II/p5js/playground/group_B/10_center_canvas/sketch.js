e6a3705be29539e5ddb35e6d26fb78c25a09e9a4
Andreas Muxel <login@andreasmuxel.com>
2020-05-10 23:36:44 +0200

    example center canvas on screen added

---
+++ Block_II/p5js/playground/group_B/10_center_canvas/sketch.js
@@ -0,0 +1,17 @@
+// preload
+function preload() {
+  // load data here
+  // for example images, sounds, JSON files..
+}
+
+// setup
+function setup() {
+  // canvas
+  createCanvas(1280, 800);
+}
+
+// draw
+function draw() {
+  // background
+  background(0, 255, 0);
+}
