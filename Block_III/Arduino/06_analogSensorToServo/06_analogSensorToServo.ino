53531d85eb8ae2d42c4ef2932cb555863b9b9a40
Andreas Muxel <login@andreasmuxel.com>
2020-05-17 19:12:24 +0200

    example analog sensor to servor motor added

--- Block_III/Arduino/06_analogSensorToServo/06_analogSensorToServo.ino
+++ Block_III/Arduino/06_analogSensorToServo/06_analogSensorToServo.ino
@@ -4,35 +4,46 @@
 // https://wiki.seeedstudio.com/Grove-Temperature_Sensor/ > look at code example
 // https://wiki.seeedstudio.com/Grove-Sound_Sensor/ > look at code example
 
-// use one of these digital actuators from your Grove Kit
-// https://wiki.seeedstudio.com/Grove-LED_Socket_Kit/
+// use servo from your Grove Kit
+// https://wiki.seeedstudio.com/Grove-Servo/
+
+// servo library
+#include <Servo.h>
 
 // These constants won't change. They're used to give names to the pins used:
 const int analogInPin = 0; // or A0  // digital input pin that the sensor is attached to
-const int analogOutPin = 6;  // analog output pin (PWM) that the actuator is attached to (PWM just works on pin 3,5,6,9,10,11)
+const int servoPin = 7; // digital pin that servor motor is attahced to
 
 int sensorValue = 0;        // value read from sensor
-int pwmValue = 0;           // pwm value used to drive actuator
+int servoValue = 0;           // value used to drive servo motor
+
+Servo servoMotor;  // create servo object to control a servo
 
 void setup() {
   // initialize serial communications at 9600 bps:
   Serial.begin(9600);
-  
-  // for analog input + output pin no initialization as an input needed !
+
+  // for analog input no initialization as an input needed !
+
+  // attaches the servo on servo pin to the servo object
+  servoMotor.attach(servoPin);
 }
 
 void loop() {
-  
+
   // read the analog pin
   sensorValue = analogRead(analogInPin);
-  
-  // map value
-  pwmValue = map(sensorValue,0,1023,0,255); // change range for sensor value for different sensors (rotary, light, ...)
+
+  // map value (sensor value to servo value)
+  // sensor: 0-1023
+  // servo: 0-180
+  servoValue = map(sensorValue, 0, 1023, 0, 180); // change range for sensor value for different sensors (rotary, light, ...)
 
   // print the results to the Serial Monitor:
   Serial.println(sensorValue); // if you use a rotary angly sensor (potentiometer) you should get values between '0' or '1023'
 
-  // switch digital actuator on/off
-  analogWrite(analogOutPin,pwmValue);
-  
+  // tell servo to go to position
+  servoMotor.write(servoValue);
+
+
 }
